a6cdec0cf7b196facc89ceb8c211961c5f118139
Md Imran Hossain <mdimranhossain.ist@gmail.com>
2025-05-11 01:28:52 +0600

    clerk add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
-import { Button } from "antd";
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
-    <div className="flex gap-4 mt-4 px-4">
-      <Button type="primary">Primary Button</Button>
-      <Button>Default Button</Button>
-      <Button type="dashed">Dashed Button</Button>
-      <Button type="text">Text Button</Button>
-      <Button type="link">Link Button</Button>
+    <div className="flex items-center justify-center h-screen">
+      <UserButton />
     </div>
   );
 }
