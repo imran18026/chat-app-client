aeca11ce60844626bd092fc6617a4a5a89d279a8
Md Imran Hossain <mdimranhossain.ist@gmail.com>
2025-05-12 03:32:57 +0600

    basic style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Oswald } from "next/font/google";
 import "./globals.css";
 import ThemeProvider from "@/providers/theme-provider";
 import { ClerkProvider } from "@clerk/nextjs";
 
-const geistSans = Geist({
+const oswald = Oswald({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
@@ -27,9 +22,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
+        <body className={`${oswald.variable} antialiased`}>
           <ThemeProvider>{children}</ThemeProvider>
         </body>
       </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import { UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
 
-export default function Home() {
+export default async function Home() {
+  const user = await currentUser();
+  console.log(user);
   return (
     <div className="flex items-center justify-center h-screen">
       <UserButton />
+      <span className="ml-10">
+        {user?.firstName} {user?.lastName} ( {user?.username} )
+      </span>
     </div>
   );
 }
